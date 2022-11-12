94b40172f0c31f14b18e5e18f23f405f9d5d522b
Oskar Eriksson <oskeri@raysearchlabs.com>
2022-11-12 07:26:36 +0100

    Use URL instead of selecting image

--- sheetmagic/index.js
+++ sheetmagic/index.js
@@ -1,42 +1,37 @@
-const state = {};
+const state = { sheetWidth: null, sheetHeight: null, sheetImage: null };
 
-const $filePicker = document.getElementById('file-picker');
-const $code = document.getElementById('code');
+const $sheetImageUrl = document.getElementById('sheet-image-url');
 const $iframe = document.getElementById('iframe');
-const $$createAttrButtons = document.querySelectorAll('.create-attr');
-
 const $sheet = $iframe.contentDocument.getElementById('sheet');
 
-const onFilePicked = (e) => {
-  const file = e.target.files[0];
-
+const setImageUrl = async () => {
+  const imageUrl = $sheetImageUrl.value;
+  const response = await fetch(imageUrl);
+  const blob = await response.blob();
   const reader = new FileReader();
-  reader.onload = (readerEvent) => {
-    const dataUrl = readerEvent.target.result;
-
+  reader.onload = function () {
     const image = new Image();
     image.onload = function () {
+      $iframe.width = this.naturalWidth;
+      $iframe.height = this.naturalHeight;
       $sheet.style.width = `${this.naturalWidth}px`;
       $sheet.style.height = `${this.naturalHeight}px`;
+      $sheet.style.backgroundImage = `url(${imageUrl})`;
     };
-    image.src = dataUrl;
-
-    $sheet.style.backgroundImage = `url(${dataUrl})`;
-    $sheet.style.backgroundSize = 'contain';
-
-    $code.textContent = $sheet.outerHTML;
+    image.src = this.result;
   };
-
-  reader.readAsDataURL(file);
+  reader.readAsDataURL(blob);
 };
 
-$filePicker.onchange = onFilePicked;
-
-function createSingleLineText() {
+const createSingleLineText = () => {
   alert('Clicked!');
-}
+};
 
-// Print all IDs
+const copyHtml = () => {
+  navigator.clipboard.writeText($sheet.outerHTML);
+};
+
+/* --- Development --- */
 let s = '';
 document.querySelectorAll('[id]').forEach((el) => {
   const { id } = el;
@@ -44,3 +39,6 @@ document.querySelectorAll('[id]').forEach((el) => {
   s += `const $${name} = document.getElementById('${id}')\n`;
 });
 console.log(s);
+
+$sheetImageUrl.value = 'https://i.ibb.co/25t9dLN/mazes-and-minotaurs.jpg';
+$sheetImageUrl.dispatchEvent(new Event('change'));
