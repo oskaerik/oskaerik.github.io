2a4729ce7c0ea0fc329334a77edc3c08ef2930d9
Oskar Eriksson <oskeri@raysearchlabs.com>
2022-11-13 08:22:53 +0100

    Propertly implement props

--- sheetmagic/index.js
+++ sheetmagic/index.js
@@ -1,12 +1,31 @@
-const $sheetImageUrl = document.getElementById('sheet-image-url');
-const $newAttribute = document.getElementById('new-attribute');
-const $copyHtml = document.getElementById('copy-html');
-const $iframe = document.getElementById('iframe');
-const $drag = document.getElementById('drag');
-const $sheet = $iframe.contentDocument.getElementsByClassName('sheet')[0];
+const PROP_TYPES = {
+  num: {
+    tag: 'input',
+    type: 'number',
+    classList: ['sm-prop', 'sm-num'],
+  },
+  slt: {
+    tag: 'input',
+    type: 'text',
+    classList: ['sm-prop', 'sm-slt'],
+  },
+  mlt: {
+    tag: 'textarea',
+    type: null,
+    classList: ['sm-prop', 'sm-mlt'],
+  },
+};
 
-const state = {};
-const allowedKeys = new Set(['imageUrl']);
+const $sheetImageUrl = document.getElementById('sheet-image-url');
+const $propType = document.getElementById('prop-type');
+const $addProp = document.getElementById('add-prop');
+const $iframe = document.getElementById('iframe');
+const $copyHtml = document.getElementById('copy-html');
+const $drag = document.getElementById('drag');
+const $sheet = $iframe.contentDocument.getElementsByClassName('sheetmagic')[0];
+
+const state = { properties: [] };
+const allowedKeys = new Set(['imageUrl', 'properties']);
 
 function saveState() {
   const queryString = new URLSearchParams({
@@ -26,17 +45,25 @@ function loadState() {
   $sheetImageUrl.value = getImageUrl() || '';
   $sheetImageUrl.dispatchEvent(new Event('change'));
 
-  if (!('x1' in state)) return;
-  const el = document.createElement('input');
-  el.classList.add('property', 'num');
-  el.type = 'number';
-  el.style.left = `${state.x1}px`;
-  el.style.top = `${state.y1}px`;
-  el.style.width = `${state.w1}px`;
-  el.style.height = `${state.h1}px`;
-  el.style.position = 'absolute';
-  $sheet.appendChild(el);
-  el.select();
+  state.properties.forEach((prop) => {
+    console.log('prop.type:', prop.type);
+    console.log('PROP_TYPES[prop.type]:', PROP_TYPES[prop.type]);
+    const { tag, type, classList } = PROP_TYPES[prop.type];
+    if (!tag) return;
+    const el = document.createElement(tag);
+    el.type = type;
+    el.classList = classList.join(' ');
+    el.style = prop.style;
+    console.log(prop.style);
+    el.style.left = `${prop.x}px`;
+    el.style.top = `${prop.y}px`;
+    el.style.width = `${prop.w}px`;
+    el.style.height = `${prop.h}px`;
+    el.style.position = 'absolute';
+    $sheet.appendChild(el);
+    el.select();
+    $propType.value = prop.type;
+  });
 }
 
 function setImageUrl(imageUrl) {
@@ -49,7 +76,7 @@ function getImageUrl() {
 }
 
 $sheetImageUrl.addEventListener('change', setSheetImage);
-$newAttribute.addEventListener('click', beginNewAttribute);
+$addProp.addEventListener('click', initCreateProp);
 $copyHtml.addEventListener('click', copyHtml);
 
 async function setSheetImage() {
@@ -73,15 +100,15 @@ async function setSheetImage() {
   reader.readAsDataURL(blob);
 }
 
-function beginNewAttribute() {
-  $sheet.addEventListener('mousedown', beginDrag);
+function initCreateProp() {
+  $sheet.addEventListener('mousedown', initDrag);
   $sheet.addEventListener('mouseup', endDrag);
   $sheet.style.cursor = 'crosshair';
 }
 
 const dragBox = { x1: null, y1: null, x2: null, y2: null };
 
-function beginDrag(ev) {
+function initDrag(ev) {
   $sheet.addEventListener('mousemove', drag);
   dragBox.x1 = ev.clientX;
   dragBox.y1 = ev.clientY;
@@ -94,10 +121,11 @@ function drag(ev) {
   const bodyRect = document.body.getBoundingClientRect();
   const offsetX = iframeRect.left - bodyRect.left;
   const offsetY = iframeRect.top - bodyRect.top;
+  const border = 2;
   const xMin = Math.min(dragBox.x1, dragBox.x2) + offsetX;
-  const xMax = Math.max(dragBox.x1, dragBox.x2) + offsetX;
+  const xMax = Math.max(dragBox.x1, dragBox.x2) + offsetX - 2 * border;
   const yMin = Math.min(dragBox.y1, dragBox.y2) + offsetY;
-  const yMax = Math.max(dragBox.y1, dragBox.y2) + offsetY;
+  const yMax = Math.max(dragBox.y1, dragBox.y2) + offsetY - 2 * border;
   $drag.style.left = `${xMin}px`;
   $drag.style.top = `${yMin}px`;
   $drag.style.width = `${xMax - xMin}px`;
@@ -106,24 +134,34 @@ function drag(ev) {
 }
 
 function endDrag() {
-  $sheet.removeEventListener('mousedown', beginDrag);
+  $sheet.removeEventListener('mousedown', initDrag);
   $sheet.removeEventListener('mouseup', endDrag);
   $sheet.removeEventListener('mousemove', drag);
   $sheet.style.cursor = 'initial';
   $drag.classList.toggle('hidden', true);
-  createProperty();
+  createProp();
 }
 
-function createProperty() {
+function createProp() {
   for (const value of Object.values(dragBox)) if (value === null) return;
   const xMin = Math.min(dragBox.x1, dragBox.x2);
   const xMax = Math.max(dragBox.x1, dragBox.x2);
   const yMin = Math.min(dragBox.y1, dragBox.y2);
   const yMax = Math.max(dragBox.y1, dragBox.y2);
-  state.x1 = xMin;
-  state.y1 = yMin;
-  state.w1 = xMax - xMin;
-  state.h1 = yMax - yMin;
+  const x = xMin;
+  const y = yMin;
+  const w = xMax - xMin;
+  const h = yMax - yMin;
+  prop = {
+    type: $propType.value,
+    x: x,
+    y: y,
+    w: w,
+    h: h,
+  };
+  if ($propType.value === 'num' || $propType.value === 'slt')
+    prop.style = `font-size: ${Math.round(0.6 * h)}px;`;
+  state.properties.push(prop);
   saveState();
 }
 
@@ -135,26 +173,12 @@ function copyHtml() {
 loadState();
 const sheetStyle = document.createElement('style');
 sheetStyle.innerHTML = `
-.sheet {
+.sheetmagic {
   position: relative;
   margin: auto;
 }
 
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-input[type='number'] {
-  -moz-appearance: textfield;
-}
-
-button[type='roll']::before {
-  content: '' !important;
-}
-
-.property {
+.sheetmagic .sm-prop {
   position: absolute;
   background: transparent !important;
   box-shadow: none;
@@ -162,12 +186,34 @@ button[type='roll']::before {
   color: black;
 }
 
-.num {
+.sheetmagic .sm-prop:focus {
+  background: rgba(0, 0, 0, 0.1) !important;
+}
+
+.sheetmagic .sm-mlt {
+  font-size: 16px;
+}
+
+.sheetmagic .sm-num {
   text-align: center;
 }
 
-.roll {
+.sheetmagic .sm-roll {
   cursor: pointer;
+}
+
+.sheetmagic input::-webkit-outer-spin-button,
+.sheetmagic input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+.sheetmagic input[type='number'] {
+  -moz-appearance: textfield;
+}
+
+.sheetmagic button[type='roll']::before {
+  content: '' !important;
 }
 `;
 $iframe.contentDocument.getElementsByTagName('head')[0].appendChild(sheetStyle);
