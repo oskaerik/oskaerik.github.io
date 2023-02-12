db0d5aab7231c062bf1d209d0e8ba42c7b71d265
Oskar Eriksson <oskaerik@gmail.com>
2023-02-12 08:36:28 +0000

    User pool config

--- wildrydes/js/config.js
+++ wildrydes/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_9hnC6pgVR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2vrnnak3v58jtmsi7t889vojpa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
